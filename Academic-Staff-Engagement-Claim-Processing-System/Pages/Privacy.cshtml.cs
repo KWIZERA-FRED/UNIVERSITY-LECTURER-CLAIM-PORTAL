b8d2632b419a8903cbc72f71bf576862ab05b5ea
malazmohdibrahim <malazmohd6@gamil.com>
2026-08-21 11:41:53 +0200

    inital commit

---
+++ Academic-Staff-Engagement-Claim-Processing-System/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Academic_Staff_Engagement_Claim_Processing_System.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
