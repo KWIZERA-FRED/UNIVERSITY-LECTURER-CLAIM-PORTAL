fa1c481b587dadccfc3af76343796b0583cebbae
malazmohdibrahim <malazmohd6@gamil.com>
2026-08-21 13:11:00 +0200

    dashboards

---
+++ Academic-Staff-Engagement-Claim-Processing-System/Pages/HOD/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Academic_Staff_Engagement_Claim_Processing_System.Pages.HOD
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Academic-Staff-Engagement-Claim-Processing-System/Pages/Lecturer/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Academic_Staff_Engagement_Claim_Processing_System.Pages.Lecturer
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
